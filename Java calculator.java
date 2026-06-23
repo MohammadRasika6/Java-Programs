d3f3c5d8b121947c01aa0cc78eff5330927d3db8
Rasika Begum <rasikabegum5@gmail.com>
2026-06-23 23:16:04 +0530

    Implement basic arithmetic operations in Java

---
+++ Java calculator.java
@@ -0,0 +1,15 @@
+public class m {
+
+	public static void main(String[] args) {
+		var a = 20;
+		var b = 10;
+		var sum = a + b;
+		var difference = a - b;
+		var product = a * b;
+		var quotient = a / b;
+		System.out.println("Sum of a and b is " + sum);
+		System.out.println("Difference of a and b is " + difference);
+		System.out.println("Product of a and b is " + product);
+		System.out.println("Quotient of a and b is " + quotient);
+  }
+}
