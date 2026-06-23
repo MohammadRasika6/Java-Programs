2e6d372a328c807a44012179613230a8d8b7cb17
Rasika Begum <rasikabegum5@gmail.com>
2026-06-23 23:32:16 +0530

    Implement relational operators in Relational.java

---
+++ Relational.java
@@ -0,0 +1,19 @@
+public class m {
+
+	public static void main(String[] args) {
+		var a = 20;
+		var b = 10;
+		var result1 = a == b;
+		var result2 = a < b;
+		var result3 = a > b;
+		var result4 = a >= b;
+		var result5 = a <= b;
+		var result6 = a != b;
+		System.out.println(result1);
+		System.out.println(result2);
+		System.out.println(result3);
+		System.out.println(result4);
+		System.out.println(result5);
+		System.out.println(result6);
+  }
+}
