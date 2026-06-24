18a61182efeb90ee8da1ccf2ac1783be61a46b1e
Rasika Begum <rasikabegum5@gmail.com>
2026-06-24 20:20:03 +0530

    Add Car class with color method and main method

---
+++ Classes.java
@@ -0,0 +1,11 @@
+class Car {
+  void color() {
+    System.out.println("The color of the car is nice");
+  }
+}
+public class Main{
+  public static void main(String[] args) {
+    Car c = new Car();
+    c.color();
+  }
+}
