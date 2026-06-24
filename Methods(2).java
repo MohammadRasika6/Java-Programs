ea1a58b9166a881aae9998d6fb9973b584080f4d
Rasika Begum <rasikabegum5@gmail.com>
2026-06-24 19:54:29 +0530

    Add method to calculate and print sum of two numbers

---
+++ Methods(2).java
@@ -0,0 +1,9 @@
+public class m {
+	    
+public static void num(int a, int b) {
+	System.out.println("The sum of a nad b is: " + (a + b));
+}
+	public static void main(String[] args) {
+		num(20, 30);
+  }
+}
