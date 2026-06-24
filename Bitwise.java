813c24940cfb6b5dac332d35ba49d7e79631785a
Rasika Begum <rasikabegum5@gmail.com>
2026-06-24 14:44:57 +0530

    showed the Bitwise.java

---
+++ Bitwise.java
@@ -0,0 +1,17 @@
+public class m {
+
+	public static void main(String[] args) {
+		var a = 30; 
+		var b = 10;
+		var c = a & b;
+		var d = a | b;
+		var e = a ^ b;
+		var f = a >> b;
+		var g = a << b;
+		System.out.println("The result of c is: " + c + ".");
+		System.out.println("The result of d is: " + d + ".");
+		System.out.println("The result of e is: " + e + ".");
+		System.out.println("The result of f is: " + f + ".");
+		System.out.println("The result of g is: " + g + ".");
+  }
+}
