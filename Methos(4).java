40a4ae3edf9d1575aa343e1321700bb8a859083f
Rasika Begum <rasikabegum5@gmail.com>
2026-06-24 20:09:09 +0530

    Create Methos(4).java with main and boolean method

---
+++ Methos(4).java
@@ -0,0 +1,11 @@
+public class m {
+	    
+
+	public static void main(String[] args) {
+		
+		System.out.println(getbooleanValue());
+	}
+		public static boolean getbooleanValue() {
+			return false;
+		}
+  }
