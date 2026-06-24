a385969e8290dec02c6d699b9d215f41d6ec4c09
Rasika Begum <rasikabegum5@gmail.com>
2026-06-24 14:39:27 +0530

    Created Logical.java

---
+++ Logical.java
@@ -0,0 +1,13 @@
+public class m {
+
+	public static void main(String[] args) {
+		var isFestival = true;
+		var isBandh = false;;
+		var Result1 = isFestival && isBandh;
+		var Result2 = isFestival || isBandh;
+		
+		System.out.println("Today is holiday because of this reason: " + Result1 + ".");
+		System.out.println("Today is holiday because of this reason: " + Result2 + ".");
+		
+  }
+}
