bf0839c34622e64f171a0381c1a57615f83a32bd
Rasika Begum <rasikabegum5@gmail.com>
2026-06-24 14:53:34 +0530

    Create DataTypes.java

---
+++ DataTypes.java
@@ -0,0 +1,21 @@
+public class m {
+
+	public static void main(String[] args) {
+		byte b = 1;
+		short s = 10;
+		int i = 100;
+		long l = 10000l;
+		float f = 3.14f;
+		double d = 454.67;
+		char c = 'R';
+		boolean bo = true;
+		System.out.println("I got " + b + "'st rank in the class.");
+		System.out.println("The cost of the pen is " + s + ".");
+		System.out.println("My cousin got " + i + "th rank in NEET.");
+		System.out.println("The interest of loan is " + l + " per year.");
+		System.out.println("The value of pi is " + f + ".");
+		System.out.println("The total marks in the semister is " + d + ".");
+		System.out.println("My name starts with " + c + ".");
+		System.out.println("Is this weather is hot!" + bo + ".");
+  }
+}
