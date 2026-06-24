7edff4a8cdbe8e6d420d72a221e5343a938c27ad
Rasika Begum <rasikabegum5@gmail.com>
2026-06-24 20:25:06 +0530

    Add Book class with prize method

---
+++ Classes2.java
@@ -0,0 +1,11 @@
+class Book {
+  void prize() {
+    System.out.println("The prize of this book is very high"):
+      }
+}
+public class Main{
+  public static void main(String[] args) {
+         Book b = new Book();
+    b.prize();
+  }
+}
