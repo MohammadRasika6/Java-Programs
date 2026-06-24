3348c58c0c1408f9b7107dec152da47cbee26aa3
Rasika Begum <rasikabegum5@gmail.com>
2026-06-24 20:04:03 +0530

    Add Method class with quotient calculation

---
+++ Method(3).java
@@ -0,0 +1,11 @@
+public class m {
+	    
+
+	public static void main(String[] args) {
+		
+		System.out.println(quotient(7.0f , 6.0f));
+	}
+		public static float quotient(float a, float b) {
+			return a/b;
+		}
+  }
