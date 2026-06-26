4ffb4be10a818bb7144bbc558f4518f1d5b8cf9f
Rasika Begum <rasikabegum5@gmail.com>
2026-06-26 23:11:58 +0530

    Add Main class with ice cream taste check

---
+++ If satement.java
@@ -0,0 +1,14 @@
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int iceCream = 20;
+		if (iceCream > 20) {
+			System.out.println("The taste of the icecream is good");
+		}
+		System.out.println("Yammy IceCream");
+
+	}
+
+}
+
