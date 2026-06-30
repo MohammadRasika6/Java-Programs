966bb1f2b8cfab4fe4e5cc04caa6256e045321ee
Rasika Begum <rasikabegum5@gmail.com>
2026-06-30 16:19:57 +0530

    Add Java program to calculate sum of first 10 numbers

---
+++ for.java
@@ -0,0 +1,12 @@
+public class sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum = 0; 
+		
+		for (int i = 1; i <= 10; i++) {
+			sum += i;
+		}
+		System.out.println("Sum of the first natural numbers: " + sum);
+		 }
+	}
